fece9f37f97b168f0a8d879d6a27119d36b68376
Kazumi Inada <hello@nandenjin.com>
2021-03-09 14:15:38 +0900

    chore: sequelizeの削除

--- src/v2/models/Organization.ts
+++ src/v2/models/Organization.ts
@@ -1,27 +1,5 @@
-import { DataTypes, Model, Optional } from 'sequelize'
 import { IOrganization } from '../types'
-import { sequelize } from '.'
 
-// Sequelized Model Basics https://sequelize.org/master/manual/model-basics.html
-// Sequelized TypeScript Guide https://sequelize.org/master/manual/typescript.html
-
-type OrganizationAttributes = IOrganization
-type OrganizationCreationAttributes = Optional<OrganizationAttributes, 'id'>
-export class Organization
-  extends Model<OrganizationAttributes, OrganizationCreationAttributes>
-  implements IOrganization {
+export class Organization implements IOrganization {
   id!: string
 }
-
-Organization.init(
-  {
-    id: {
-      type: DataTypes.STRING(32),
-      primaryKey: true,
-    },
-  },
-  {
-    sequelize,
-    tableName: 'organizations',
-  }
-)
